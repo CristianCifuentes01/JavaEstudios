c63544514b40dc5aabc3a4bb329de3e1d60c1a5c
CristianCifuentes01 <124294094+CristianCifuentes01@users.noreply.github.com>
2023-03-01 12:30:04 -0500

    Update Pruebas.java

--- Java/Programa1Estructura/src/poo/Pruebas.java
+++ Java/Programa1Estructura/src/poo/Pruebas.java
@@ -3,16 +3,21 @@ package poo;
 public class Pruebas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		Empleados trabajador1=new Empleados("Cristian");
 		Empleados trabajador2=new Empleados("Andres");
-		
-		
+		Empleados trabajador3=new Empleados("Antonio");
+		Empleados trabajador4=new Empleados("Clara");
 		
 		trabajador2.cambiaSeccion("Recursos Humanos");
-		System.out.println(trabajador1.devuelveDatos());
-		System.out.println(trabajador2.devuelveDatos());
+		
+		
+		System.out.println(trabajador1.devuelveDatos()+"\n"
+		+trabajador2.devuelveDatos()+"\n"+trabajador3.devuelveDatos()+"\n"
+		+trabajador4.devuelveDatos());
+		System.out.println(Empleados.dameIdSguiente());
+		
 	}
 
 	
@@ -20,20 +25,32 @@ public class Pruebas {
 
 class Empleados{
 	
+	
 	private final String nombre;
 	private String seccion;
+	private int Id;
+	private static int IdSiguiente=1;
+	
+	
 	public Empleados(String nom) {//CONSTRUCTOR 
 		nombre=nom;
 		seccion = "Administración";
+		Id=IdSiguiente;
+		IdSiguiente++;
 	}
+	
 	
 	public void cambiaSeccion(String seccion) {//METODO SETTER
-		this.seccion=seccion;
-		
-	}
-	public String devuelveDatos() {//METODO GETTER
-		return "El nombre es: "+nombre+ ". Seccion es: "+seccion;
+		this.seccion=seccion;	
 	}
 	
+	
+	public String devuelveDatos() {//METODO GETTER
+		return "El nombre es: "+nombre+ ". \nLa seccion es: "+seccion+" \nEl ID es: "+Id+"\n";
+	}
+	
+	public static String dameIdSguiente() {
+		return "El ID siguiente es: "+IdSiguiente;
+	}
 	
 }
